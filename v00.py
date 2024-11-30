29ec54fe9687d32c8eb6d0997fdde77fe6849118
anavop <ana.vasconcellos00@gmail.com>
2024-11-30 20:37:04 -0300

    Mensagem do commit

--- v00.py
+++ v00.py
@@ -1,7 +1,14 @@
 import os
+import random 
+
+potencia = random.seed(42)
+numero = random.seed(42)
+digt4 = (potencia**numero)
+
+digt1 = (numero % 9)
+digt2 = (potencia % 9)
+digt3 = (numero % 9)
+digt4 = (digt4 % 9)
 
 
-potencia 
-numero
-numero base 
-digt4 
+print(digt1 + ' ' + digt2 + ' ' + digt3 + ' ' + digt4)
